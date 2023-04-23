d7a233dca5081c6d1c15a2fd2b4356339e36f8d4
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-23 15:17:55 +0300

    Модуль03_Лекция01_С#_программа ветвления_проверка имени пользователя

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
-﻿// программа нахождения остатка от деления через = new Random().Next(min,max);
-int numberA = new Random().Next(11, 99);// - любое число от 1 до 9
-Console.WriteLine("Рандомное число полученное командой =new Random().Next(11,99)   равно " + numberA);
-Console.WriteLine("Остаток от деления на 3: " + (numberA%3));
+﻿// программа ввести имя,если имя==Alex, поприветсвовать/иначе послать
+string userName = "Alex";
+Console.WriteLine("Введите имя пользователя: ");
+string input_userName = Console.ReadLine();
+if (input_userName==userName)
+{
+    Console.Write("Ура! Это же Саня!");
+}
+else
+{
+    Console.Write("Вы кто такие? Я Вас не звал...");
+}
