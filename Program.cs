9175da2db25f7af571f597a9adaef66ffd580535
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-23 15:24:25 +0300

    Модуль03_Лекция01_С#_программа деления двух чисел типа double!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// программа сложения двух чисел
+﻿// программа сложения двух чисел d 
 Console.Write("Введите первое число ");
 double numberA = double.Parse(Console.ReadLine());
 Console.Write("Введите второе число ");
