1b02f7755229cc9366ff5081d4bb640efd18a142
Alecsander Zdanevich <alecsdjkzda@gmail.com>
2023-04-23 14:53:17 +0300

    Модуль03_Лекция01_С#_программа нахождения остатка от деления с использованием random

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// программа сложения двух чисел
-Console.Write("Введите первое число ");
-double numberA = double.Parse(Console.ReadLine());
-Console.Write("Введите второе число ");
-double numberB = double.Parse(Console.ReadLine());
-Console.Write("Результат деления равен " + (numberA / numberB));
+﻿// программа нахождения остатка от деления через = new Random().Next(min,max);
+int numberA = new Random().Next(11, 99);// - любое число от 1 до 9
+Console.WriteLine("Рандомное число полученное командой =new Random().Next(11,99)   равно " + numberA);
+Console.WriteLine("Остаток от деления на 3: " + (numberA%3));
